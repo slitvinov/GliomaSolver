c9b0e173ae489b5760e270066ef1089f6f728bc0
Sergey Litvinov <slitvinov@gmail.com>
2023-07-31 16:05:12 +0200

    up

--- likelihood/main.cpp
+++ likelihood/main.cpp
@@ -1,5 +1,5 @@
 //
-//  HGG_Likelihood.cpp
+//  Likelihood_main.cpp
 //  GliomaXcode
 //
 //  Created by Lipkova on 15/06/15.
@@ -7,6 +7,7 @@
 //
 
 #include "HGG_Likelihood.h"
+
 
 HGG_Likelihood::HGG_Likelihood(const int argc, const char **argv)
     : parser(argc, argv) {
@@ -171,3 +172,27 @@ void HGG_Likelihood::run() {
   printf("LogLike = %Lf \n", costFunction);
   _writeToFile(costFunction);
 }
+
+double my_gettime() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1.0E-6;
+}
+
+int main(int argc, const char **argv) {
+  printf("==========================================\n");
+  printf("          Computing likelihood            \n");
+  printf("==========================================\n");
+
+  ArgumentParser parser(argc, argv);
+  double t_begin = my_gettime();
+
+  HGG_Likelihood *l = new HGG_Likelihood(argc, (const char **)argv);
+  l->run();
+  delete l;
+
+  double t_end = my_gettime();
+  printf("Likelihood: elapsed time=%f (sec) \n", t_end - t_begin);
+
+  return 0;
+};
