e61fb285a3e04a87a5e53cf9227eab5c26c45844
Sergey Litvinov <slitvinov@gmail.com>
2023-07-31 15:15:32 +0200

    add likelihood/makefile/LikelihoodInput

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ N=4
 OMP_NUM_THREADS=$N LD_LIBRARY_PATH=VTK/lib/vtk-5.2 "$brain" \
     -adaptive 1 \
     -dumpfreq 50 \
-    -Dw 0.0013 \
     -icx 0.28 \
     -icy 0.75 \
     -icz 0.35 \
@@ -13,7 +12,6 @@ OMP_NUM_THREADS=$N LD_LIBRARY_PATH=VTK/lib/vtk-5.2 "$brain" \
     -nthreads $N \
     -PatFileName ./ \
     -profiler 1 \
-    -rho 0.025 \
-    -Tend 300 \
+    -UQ 1 \
     -verbose 1 \
     -vtk 1 \
