2f3eae7dac5058cb3f63865e43ddd4cfbde7f138
Sergey Litvinov <slitvinov@gmail.com>
2023-08-01 12:12:40 +0200

    ic[xyz] in TumorIC.txt

--- run.sh
+++ run.sh
@@ -6,9 +6,6 @@ N=4
 OMP_NUM_THREADS=$N LD_LIBRARY_PATH=VTK/lib/vtk-5.2 "$brain" \
     -adaptive 1 \
     -dumpfreq 50 \
-    -icx 0.28 \
-    -icy 0.75 \
-    -icz 0.35 \
     -model RD \
     -nthreads $N \
     -PatFileName ./ \
