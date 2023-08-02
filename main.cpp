662b68fd12daf9ea76b6066a0f6ff5c26da5dd40
Sergey Litvinov <slitvinov@gmail.com>
2023-08-02 13:59:23 +0200

    clang-format -i

--- main.cpp
+++ main.cpp
@@ -142,6 +142,7 @@ private:
     mNelements = nx * ny * nz;
     mData = new T[mNelements];
   }
+
 public:
   size_t getSizeX() const { return mNx; }
   size_t getSizeY() const { return mNy; }
@@ -248,63 +249,56 @@ struct ReactionDiffusionOperator {
     Real df_loc; // diffusion at the current point (local)
     Real chf_loc; // diffusion at the current point (local)
 
-      for (int iz = 0; iz < BlockType::sizeZ; iz++)
-        for (int iy = 0; iy < BlockType::sizeY; iy++)
-          for (int ix = 0; ix < BlockType::sizeX; ix++) {
-            // check if we are in the brain domain
-            if (lab(ix, iy, iz).p_w + lab(ix, iy, iz).p_g +
-                    lab(ix, iy, iz).phi >
-                0.) {
-              df_loc = lab(ix, iy, iz).p_w * Dw + lab(ix, iy, iz).p_g * Dg;
-              df[0] =
-                  lab(ix - 1, iy, iz).p_w * Dw + lab(ix - 1, iy, iz).p_g * Dg;
-              df[1] =
-                  lab(ix + 1, iy, iz).p_w * Dw + lab(ix + 1, iy, iz).p_g * Dg;
-              df[2] =
-                  lab(ix, iy - 1, iz).p_w * Dw + lab(ix, iy - 1, iz).p_g * Dg;
-              df[3] =
-                  lab(ix, iy + 1, iz).p_w * Dw + lab(ix, iy + 1, iz).p_g * Dg;
-              df[4] =
-                  lab(ix, iy, iz - 1).p_w * Dw + lab(ix, iy, iz - 1).p_g * Dg;
-              df[5] =
-                  lab(ix, iy, iz + 1).p_w * Dw + lab(ix, iy, iz + 1).p_g * Dg;
+    for (int iz = 0; iz < BlockType::sizeZ; iz++)
+      for (int iy = 0; iy < BlockType::sizeY; iy++)
+        for (int ix = 0; ix < BlockType::sizeX; ix++) {
+          // check if we are in the brain domain
+          if (lab(ix, iy, iz).p_w + lab(ix, iy, iz).p_g + lab(ix, iy, iz).phi >
+              0.) {
+            df_loc = lab(ix, iy, iz).p_w * Dw + lab(ix, iy, iz).p_g * Dg;
+            df[0] = lab(ix - 1, iy, iz).p_w * Dw + lab(ix - 1, iy, iz).p_g * Dg;
+            df[1] = lab(ix + 1, iy, iz).p_w * Dw + lab(ix + 1, iy, iz).p_g * Dg;
+            df[2] = lab(ix, iy - 1, iz).p_w * Dw + lab(ix, iy - 1, iz).p_g * Dg;
+            df[3] = lab(ix, iy + 1, iz).p_w * Dw + lab(ix, iy + 1, iz).p_g * Dg;
+            df[4] = lab(ix, iy, iz - 1).p_w * Dw + lab(ix, iy, iz - 1).p_g * Dg;
+            df[5] = lab(ix, iy, iz + 1).p_w * Dw + lab(ix, iy, iz + 1).p_g * Dg;
 
-              _harmonic_mean(df, df_loc);
+            _harmonic_mean(df, df_loc);
 
-              chf[0] = lab(ix - 1, iy, iz).phi + lab(ix - 1, iy, iz).p_w +
-                       lab(ix - 1, iy, iz).p_g;
-              chf[1] = lab(ix + 1, iy, iz).phi + lab(ix + 1, iy, iz).p_w +
-                       lab(ix + 1, iy, iz).p_g;
-              chf[2] = lab(ix, iy - 1, iz).phi + lab(ix, iy - 1, iz).p_w +
-                       lab(ix, iy - 1, iz).p_g;
-              chf[3] = lab(ix, iy + 1, iz).phi + lab(ix, iy + 1, iz).p_w +
-                       lab(ix, iy + 1, iz).p_g;
-              chf[4] = lab(ix, iy, iz - 1).phi + lab(ix, iy, iz - 1).p_w +
-                       lab(ix, iy, iz - 1).p_g;
-              chf[5] = lab(ix, iy, iz + 1).phi + lab(ix, iy, iz + 1).p_w +
-                       lab(ix, iy, iz + 1).p_g;
+            chf[0] = lab(ix - 1, iy, iz).phi + lab(ix - 1, iy, iz).p_w +
+                     lab(ix - 1, iy, iz).p_g;
+            chf[1] = lab(ix + 1, iy, iz).phi + lab(ix + 1, iy, iz).p_w +
+                     lab(ix + 1, iy, iz).p_g;
+            chf[2] = lab(ix, iy - 1, iz).phi + lab(ix, iy - 1, iz).p_w +
+                     lab(ix, iy - 1, iz).p_g;
+            chf[3] = lab(ix, iy + 1, iz).phi + lab(ix, iy + 1, iz).p_w +
+                     lab(ix, iy + 1, iz).p_g;
+            chf[4] = lab(ix, iy, iz - 1).phi + lab(ix, iy, iz - 1).p_w +
+                     lab(ix, iy, iz - 1).p_g;
+            chf[5] = lab(ix, iy, iz + 1).phi + lab(ix, iy, iz + 1).p_w +
+                     lab(ix, iy, iz + 1).p_g;
 
-              _applyNoFluxBC(df, chf);
+            _applyNoFluxBC(df, chf);
 
-              // diffusion fluxes
-              double diffusionFluxIn = ih2 * (df[0] * lab(ix - 1, iy, iz).phi +
-                                              df[1] * lab(ix + 1, iy, iz).phi +
-                                              df[2] * lab(ix, iy - 1, iz).phi +
-                                              df[3] * lab(ix, iy + 1, iz).phi +
-                                              df[4] * lab(ix, iy, iz - 1).phi +
-                                              df[5] * lab(ix, iy, iz + 1).phi);
+            // diffusion fluxes
+            double diffusionFluxIn = ih2 * (df[0] * lab(ix - 1, iy, iz).phi +
+                                            df[1] * lab(ix + 1, iy, iz).phi +
+                                            df[2] * lab(ix, iy - 1, iz).phi +
+                                            df[3] * lab(ix, iy + 1, iz).phi +
+                                            df[4] * lab(ix, iy, iz - 1).phi +
+                                            df[5] * lab(ix, iy, iz + 1).phi);
 
-              double diffusionFluxOut =
-                  -((df[0] + df[1] + df[2] + df[3] + df[4] + df[5]) *
-                    lab(ix, iy, iz).phi * ih2);
-              double reactionFlux =
-                  rho * lab(ix, iy, iz).phi * (1. - lab(ix, iy, iz).phi);
+            double diffusionFluxOut =
+                -((df[0] + df[1] + df[2] + df[3] + df[4] + df[5]) *
+                  lab(ix, iy, iz).phi * ih2);
+            double reactionFlux =
+                rho * lab(ix, iy, iz).phi * (1. - lab(ix, iy, iz).phi);
 
-              o(ix, iy, iz).dphidt =
-                  diffusionFluxOut + diffusionFluxIn + reactionFlux;
-            } else
-              o(ix, iy, iz).dphidt = 0.;
-          }
+            o(ix, iy, iz).dphidt =
+                diffusionFluxOut + diffusionFluxIn + reactionFlux;
+          } else
+            o(ix, iy, iz).dphidt = 0.;
+        }
   }
 
   // Di,j = 2 * (Di * Dj / (Di + Dj)
@@ -321,10 +315,10 @@ struct ReactionDiffusionOperator {
     df[3] =
         (df[3] + df_loc < eps) ? 0. : 2. * df[3] * df_loc / (df[3] + df_loc);
 
-      df[4] =
-          (df[4] + df_loc < eps) ? 0. : 2. * df[4] * df_loc / (df[4] + df_loc);
-      df[5] =
-          (df[5] + df_loc < eps) ? 0. : 2. * df[5] * df_loc / (df[5] + df_loc);
+    df[4] =
+        (df[4] + df_loc < eps) ? 0. : 2. * df[4] * df_loc / (df[4] + df_loc);
+    df[5] =
+        (df[5] + df_loc < eps) ? 0. : 2. * df[5] * df_loc / (df[5] + df_loc);
   }
 
   inline void _applyNoFluxBC(Real (&df)[6], Real n[6]) const {
@@ -345,12 +339,12 @@ struct ReactionDiffusionOperator {
       df[2] *= 2.0;
     }
 
-      if (n[4] < eps) {
-        df[5] *= 2.0;
-      }
-      if (n[5] < eps) {
-        df[4] *= 2.0;
-      }
+    if (n[4] < eps) {
+      df[5] *= 2.0;
+    }
+    if (n[5] < eps) {
+      df[4] *= 2.0;
+    }
   }
 };
 
@@ -363,13 +357,13 @@ struct UpdateTumor {
 
   template <typename BlockType>
   inline void operator()(const BlockInfo &info, BlockType &o) const {
-      for (int iz = 0; iz < BlockType::sizeZ; iz++)
-        for (int iy = 0; iy < BlockType::sizeY; iy++)
-          for (int ix = 0; ix < BlockType::sizeX; ix++) {
-            o(ix, iy, iz).phi += dt * o(ix, iy, iz).dphidt;
-            o(ix, iy, iz).phi = max((Real)0., o(ix, iy, iz).phi);
-            o(ix, iy, iz).phi = min((Real)1., o(ix, iy, iz).phi);
-          }
+    for (int iz = 0; iz < BlockType::sizeZ; iz++)
+      for (int iy = 0; iy < BlockType::sizeY; iy++)
+        for (int ix = 0; ix < BlockType::sizeX; ix++) {
+          o(ix, iy, iz).phi += dt * o(ix, iy, iz).dphidt;
+          o(ix, iy, iz).phi = max((Real)0., o(ix, iy, iz).phi);
+          o(ix, iy, iz).phi = min((Real)1., o(ix, iy, iz).phi);
+        }
   }
 };
 
