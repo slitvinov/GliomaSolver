01b727e2b440bfdb2d01d0922dee8a0a1d1db011
Sergey Litvinov <slitvinov@gmail.com>
2023-08-11 16:18:59 +0200

    initial import of brain_ini, brain_fin

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,25 @@
+#include "lib.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lib.h"
+
+struct Brain {};
+
+int brain_ini(int nx, int ny, int nz, float *GM, float *WM, double Dw,
+              double rho, struct Brain **pbrain) {
+  struct Brain *brain;
+  if ((brain = (struct Brain *)malloc(sizeof *brain)) == NULL) {
+    fprintf(stderr, "%s:%d: malloc failed\n", __FILE__, __LINE__);
+    return 1;
+  }
+  *pbrain = brain;
+  return 0;
+}
+
+int brain_fin(struct Brain *brain) {
+  free(brain);
+  return 0;
+}
+
 float *brain_read(const char *path, int *nx, int *ny, int *nz) {
   float *d;
   FILE *file;
@@ -21,11 +40,13 @@ float *brain_read(const char *path, int *nx, int *ny, int *nz) {
     return NULL;
   }
   if (header[1] != 3) {
-    fprintf(stderr, "%s:%d: not a three dimensional file '%s'\n", __FILE__, __LINE__, path);
+    fprintf(stderr, "%s:%d: not a three dimensional file '%s'\n", __FILE__,
+            __LINE__, path);
     return NULL;
   }
   if (header[5] != 1) {
-    fprintf(stderr, "%s:%d: not a float32 file '%s'\n", __FILE__, __LINE__, path);
+    fprintf(stderr, "%s:%d: not a float32 file '%s'\n", __FILE__, __LINE__,
+            path);
     return NULL;
   }
   n = header[2] * header[3] * header[4];
@@ -33,7 +54,8 @@ float *brain_read(const char *path, int *nx, int *ny, int *nz) {
   *ny = header[3];
   *nz = header[4];
   if ((d = (float *)malloc(n * sizeof *d)) == NULL) {
-    fprintf(stderr, "%s:%d: malloc failed for '%s'\n", __FILE__, __LINE__, path);
+    fprintf(stderr, "%s:%d: malloc failed for '%s'\n", __FILE__, __LINE__,
+            path);
     return NULL;
   }
   if (fread(d, sizeof *d, n, file) != n) {
--- lib.h
+++ lib.h
@@ -4,11 +4,11 @@ extern "C" {
 
 struct Brain;
 float *brain_read(const char *, int *, int *, int *);
-int brain_ini(int, int, int, float *, float *, double, double, struct Brain *);
+int brain_ini(int, int, int, float *, float *, double, double, struct Brain **);
 int brain_step(struct Brain *, double);
 int brain_dump(struct Brain *, char *path);
 int brain_project(struct Brain *, float *);
-int brain_fin(struct Brain*);
+int brain_fin(struct Brain *);
 
 #ifdef __cplusplus
 }
