ad3ce72ea16dfcd9f67630f0e5b432894eef1c11
Sergey Litvinov <slitvinov@gmail.com>
2023-08-11 15:31:34 +0200

    add brain_* header

--- lib.h
+++ lib.h
@@ -1,8 +1,15 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct Brain;
 float *brain_read(const char *, int *, int *, int *);
+int brain_ini(int, int, int, float *, float *, double, double, struct Brain *);
+int brain_step(struct Brain *, double);
+int brain_dump(struct Brain *, char *path);
+int brain_project(struct Brain *, float *);
+int brain_fin(struct Brain*);
+
 #ifdef __cplusplus
 }
 #endif
-  
