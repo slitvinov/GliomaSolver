c82f59c5886db4934acecb98b0e4ef2075b0553d
Sergey Litvinov <slitvinov@gmail.com>
2023-09-22 16:51:48 -0400

    rm readDat functions

--- fun.py
+++ fun.py
@@ -1,35 +1,10 @@
 #!/usr/bin/python
 #%%
 import sys
-import struct
 import glioma_solver
 import numpy as np
 import nibabel as nib
 
-
-def unpack(string, file):
-    buffer = file.read(struct.calcsize(string))
-    return struct.unpack(string, buffer)
-
-
-def readDat(path):
-    with open(path, "rb") as inp:
-        magic, = unpack('i', inp)
-        if magic != 1234:
-            sys.stderr.write("fun.py error: '%s' not a dat file\n" % path)
-            sys.exit(1)
-        dim, = unpack('i', inp)
-        if dim != 3:
-            sys.stderr.write("fun.py error: wrong dimension '%d'\n" % dim)
-            sys.exit(1)
-        nx, ny, nz, type_id = unpack('iiii', inp)
-        seek = inp.tell()
-        type_name = {0: "<d", 1: "<f", 2: "<i"}[type_id]
-    with open(path, "rb+") as inp:
-        mm = inp.read()
-        dtype = np.dtype(type_name)
-        return np.ndarray((nx, ny, nz), dtype, mm, seek, order='F')
-    
 def readNii(path):
     return nib.load(path).get_fdata().astype(np.float32)
 
