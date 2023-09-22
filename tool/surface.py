20c5ae311b01d6e5b79d83350e9809e4d84b8417
Sergey Litvinov <slitvinov@gmail.com>
2023-09-22 16:49:22 -0400

    tool/surface.py: write surface

--- tool/surface.py
+++ tool/surface.py
@@ -4,34 +4,55 @@ import sys
 import array
 import numpy as np
 import skimage.measure
+import os
+import nibabel as nib
 
+def readNii(path):
+    return nib.load(path).get_fdata()
 
-def read(path):
-    with open(path, "rb") as inp:
-        a = array.array('i')
-        a.fromfile(inp, 6)
-        magic, dim, nx, ny, nz, type_id = a
-        if magic != 1234:
-            sys.stderr.write("surface.py: error: '%s' not a dat file\n" % path)
-            sys.exit(1)
-        if dim != 3:
-            sys.stderr.write("surface.py: error: wrong dimension '%d'\n" % dim)
-            sys.exit(1)
-        seek = inp.tell()
-        type_name = {0: "<d", 1: "<f", 2: "<i"}[type_id]
-    with open(path, "rb+") as inp:
-        mm = inp.read()
-        dtype = np.dtype(type_name)
-        return np.ndarray((nx, ny, nz), dtype, mm, seek, order='F')
-
-
-a = read(sys.argv[1])
+a = readNii(sys.argv[1])
 sx, sy, sz = 1, 1, 1
 nx, ny, nz = np.shape(a)
 dx = 1.0 / nx / sx
 dy = 1.0 / ny / sy
 dz = 1.0 / nz / sz
-verts, faces, normals, values = skimage.measure.marching_cubes(a,
+verts, faces, normals, values = skimage.measure.marching_cubes(a[::sx, ::sy, ::sz],
                                                                0.5,
-                                                               spacing=(sx, sy,
-                                                                        sz))
+                                                               spacing=(dx, dy,
+                                                                        dz))
+path = "surface"
+xyz_path = "%s.xyz.raw" % path
+topo_path = "%s.topo.raw" % path
+xdmf_path = "%s.xdmf2" % path
+xyz = np.memmap(xyz_path, np.dtype("<f4"), "w+", shape=(len(verts), 3), order='C')
+topo = np.memmap(topo_path, np.dtype("<i4"), "w+", shape=(len(faces), 3), order='C')
+np.copyto(xyz, verts)
+np.copyto(topo, faces, 'no')
+with open(xdmf_path, "w") as f:
+    f.write("""\
+<Xdmf
+    Version="2">
+  <Domain>
+    <Grid>
+      <Topology
+	  TopologyType="Triangle"
+	  Dimensions="%d">
+	<DataItem
+	    Dimensions="%d 3"
+	    NumberType="Int"
+	    Format="Binary">
+          %s
+	</DataItem>
+      </Topology>
+      <Geometry>
+	<DataItem
+	    Dimensions="%d 3"
+	    Format="Binary">
+          %s
+	</DataItem>
+      </Geometry>
+    </Grid>
+  </Domain>
+</Xdmf>
+""" % (len(faces), len(faces), os.path.basename(topo_path), len(verts),
+       os.path.basename(xyz_path)))
